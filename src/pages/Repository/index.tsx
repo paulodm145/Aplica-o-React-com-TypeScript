278392f221e880c6d3fcd05529e3db4f059c3ddd
Paulo Roberto Bolsanello <paulo.bolsanello@gmail.com>
2021-01-03 16:41:37 -0300

    Conclusão da pagina de repositório e listagem de Issues. Exercício concluído

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,58 +1,115 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import logoImg from '../../assets/logo.svg';
 import { Header, RepositoryInfo, Issues } from './styled';
-import {FiChevronLeft, FiChevronRight} from 'react-icons/fi'
+import {FiChevronLeft, FiChevronRight} from 'react-icons/fi';
+import api from '../../services/api';
 
 interface RepositoryParams{
     repository: string;
 }
 
+interface Repository {
+    full_name: string;
+    description: string,
+    stargazers_count: number,
+    forks_count: number,
+    open_issues_count: number,
+    owner: {
+        login: string;
+        avatar_url: string;
+    }
+}
+
+interface Issue{
+    id: number;
+    title: string;
+    html_url: string;
+    user: {
+        login: string;
+    }
+}
+
 const Repository: React.FC = () => {
 
-    const { params } = useRouteMatch<RepositoryParams>()
+    const [repository, setRepository] = useState<Repository | null>(null); /** | significa "ou" e deve ser usando quando carregamos objetos */
+    const [issues, setIssues] = useState<Issue[]>([]); /** arrays dispensam a técnica mostrada acima, pois um array vazio ainda é um array */
+
+    const { params } = useRouteMatch<RepositoryParams>();
+
+    useEffect( () => { 
+       
+       api.get(`repos/${params.repository}`).then(response => {
+                setRepository(response.data);
+            });
+        api.get(`repos/${params.repository}/issues`).then(response => {
+                setIssues(response.data);
+            })
+        
+       /*async function loadData(): Promise<void>{
+            const [repository, issues] = await Promise.all([
+                api.get(`repos/${params.repository}`),
+                api.get(`repos/${params.repository}/issues`)
+            ]);
+
+            console.log(repository);
+            console.log(issues);
+        }*/
+
+    } , [params.repository]);
+
     return (
         <>
             <Header>
                 <img src={logoImg} alt="Github Explorer" /> 
-                <Link to='/dashboard'>
+                <Link to='/'>
                     <FiChevronLeft size={16} />
                     Voltar
                 </Link>
             </Header>
-            <RepositoryInfo>
-                <header>
-                    <img src="https://avatars0.githubusercontent.com/u/46395433?s=460&u=b006bd3112c241872fbe5496814881de4dedef29&v=4" alt="Explorer" />
-                    <div>
-                        <strong>rocketseat/unform</strong>
-                        <p>Descrição do repositório</p>
-                    </div>
-                </header>
-                <ul>
-                    <li>
-                        <strong>1808</strong>
-                        <p>Stars</p>
-                    </li>
-                    <li>
-                        <strong>48</strong>
-                        <p>Forks</p>
-                    </li>
-                    <li>
-                        <strong>67</strong>
-                        <p>Issues abertas</p>
-                    </li>
-                </ul>
-            </RepositoryInfo>
-
+      
+            { repository && (
+                      <RepositoryInfo>
+                      <header>
+                          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+                          <div>
+                              <strong>{repository.owner.login}</strong>
+                              <p>{repository.description}</p>
+                          </div>
+                      </header>
+                      <ul>
+      
+                          <li>
+                              <strong>{repository.stargazers_count}</strong>
+                              <p>Stars</p>
+                          </li>
+                          <li>
+                              <strong>{repository.forks_count}</strong>
+                              <p>Forks</p>
+                          </li>
+                          <li>
+                              <strong>{repository.open_issues_count}</strong>
+                              <p>Issues abertas</p>
+                          </li>
+                      </ul>
+                  </RepositoryInfo>
+            )}
             
             <Issues>
-                <Link  to="asas">
-                <div>
-                    <strong>Repositorio</strong>
-                    <p>descrição</p>
-                </div>
-                <FiChevronRight size={20} />
-                </Link>
+
+               { 
+                issues.map
+                    (issue => (
+                        <a key={issue.id} target='_blank' href={issue.html_url}>
+                        <div>
+                            <strong>{issue.title}</strong>
+                            <p>{issue.user.login}</p>
+                        </div>
+                        <FiChevronRight size={20} />
+                        </a>
+                    ))
+                }
+
             </Issues>
 
         </>
